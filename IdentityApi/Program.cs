211a1fa5c26cef9303cbfeb39ada83dbbb0e0bf1
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-01-06 01:38:38 +0200

    Registering dependencies

--- IdentityApi/Program.cs
+++ IdentityApi/Program.cs
@@ -1,11 +1,41 @@
 
+using IdentityApi.Domain;
+using IdentityApi.Features.CreateAccount;
+using IdentityApi.Features.GetNewToken;
+using IdentityApi.Features.LoginAccount;
+using IdentityApi.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Serevices.AddDbContext<AppDbContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnection("Default")));
 
-// Add services to the container.
+builder.Serevices.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Serevices.AddJwtAuthenticationService(builder.Configuration);
+builder.Serevices.AddGrpc().AddJsontranscoding();
+builder.Serevices.AddValidatorsFromAssembly(typeof(Program).Assembly);
+builder.Serevices.AddMediatR(c => c.RegisterServicefromAssembly(typeof(Program).Assembly));
+builder.Serevices.AddScoped(p =>
+{
+    var config = new TypeAdapterConfig();
+    CreateAccountMapperConfig.Register(config);
+    LoginAccountMapperConfig.Register(config);
+    return config;
+
+});
+
+builder.Serevices.AddScoped<IMapper, ServiceMapper>();
 builder.Services.AddGrpc();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+using(var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    var userManager = services.GetRequireService<UserManager<AppUser>>();
+    await context.Database.MigrateAsync();
+    await DatabaseSeeder.SeedAsync(context,userManager);
+}
+app.MapGrpcService<CreateAccountService>();
+app.MapGrpcService<LoginAccountService>();
+app.MapGrpcService<GetNewTokenService>();
 app.Run();
